557f68cf9803358e312b2874bbffd2e5b6e0f52c
Matthieu "Ezveus" Ciappara <ciappam@gmail.com>
2013-02-12 13:18:17 +0100

    Reading 4 more bytes to read the whole WS header.

--- lib/WEBSocket/socket.rb
+++ lib/WEBSocket/socket.rb
@@ -28,19 +28,28 @@ module WEBSocket
       end
 
       def read length = nil, buffer = nil
-        s = @socket.read length, buffer
+        s = nil
+        if length
+          if length.kind_of? Numeric
+            s = @socket.read length + 4, buffer
+          elsif buffer.nil?
+            s = @socket.read nil, length
+          end
+        else
+          s = @socket.read nil, nil
+        end
         from_frame_to_text s
       end
 
       def read_nonblock length, buffer = nil
         buffer ||= ''
 
-        @socket.read_nonblock length, buffer
+        @socket.read_nonblock length + 4, buffer
         from_frame_to_text buffer
       end
 
       def readpartial length, buffer = nil
-        s = @socket.readpartial length, buffer
+        s = @socket.readpartial length + 4, buffer
         from_frame_to_text s
       end
 
@@ -58,15 +67,15 @@ module WEBSocket
       end
 
       def write s
-        @socket.write from_text_to_frame(s)
+        @socket.write from_text_to_frame(s.to_s)
       end
 
       def write_nonblock s
-        @socket.write_nonblock from_text_to_frame(s)
+        @socket.write_nonblock from_text_to_frame(s.to_s)
       end
 
       def << s
-        @socket << from_text_to_frame(s)
+        @socket << from_text_to_frame(s.to_s)
       end
     end
   end
